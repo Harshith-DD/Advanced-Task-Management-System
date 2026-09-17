3aea4b5df8e81e69633b78d8c2a0cf4b71fbf3ee
Harshith Kumar <harshith.kumar@discoverdollar.com>
2026-09-17 16:54:29 +0530

    feat: add Angular routing and page structure

--- taskflow-angular/src/app/app.routes.ts
+++ taskflow-angular/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './dashboard/dashboard';
+import { Tasks } from './tasks/tasks';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'tasks',
+    component: Tasks,
+  },
+];
--- taskflow-angular/src/app/app.ts
+++ taskflow-angular/src/app/app.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TaskCard } from './task-card/task-card';
 @Component({
-  imports: [RouterOutlet,TaskCard],
+  imports: [RouterLink, RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
